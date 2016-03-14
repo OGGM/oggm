72987dcb074ebfc5c6d33814982229e872e64c63
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-03-14 15:03:35 +0100

    Re-add install_env script for generic pip environments

---
+++ deployment/install_env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo "Installing numpy..."
+pip install numpy
+
+echo "Installing scipy..."
+pip install scipy
+
+echo "Installing pandas shapely cython matplotlib..."
+pip install pandas shapely cython
+pip install matplotlib
+
+echo "Installing gdal fiona..."
+pip install gdal==1.10.0 --install-option="build_ext" --install-option="--include-dirs=/usr/include/gdal"
+pip install fiona --install-option="build_ext" --install-option="--include-dirs=/usr/include/gdal"
+
+echo "Installing other packages..."
+pip install pyproj rasterio Pillow geopandas netcdf4 scikit-image configobj joblib xarray nose
+
+echo "Installing git packages..."
+pip install git+https://github.com/fmaussion/motionless.git
+pip install git+https://github.com/fmaussion/salem.git
+pip install git+https://github.com/fmaussion/cleo.git
+
+echo "Done installing pip packages"
+
