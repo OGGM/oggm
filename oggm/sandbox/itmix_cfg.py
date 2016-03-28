b9c03852a95c35f8b5c2814d0362b634c492d072
Fabien Maussion <fabien.maussion@uibk.ac.at>
2016-03-28 15:11:31 +0000

    runs on aws

--- oggm/sandbox/itmix_cfg.py
+++ oggm/sandbox/itmix_cfg.py
@@ -1,13 +1,13 @@
 """Config file for ITMIX."""
 
-# DATA_DIR = '/work/ubuntu/oggm-data/'
-# WORKING_DIR = '/work/ubuntu/wd/base/'
-# PLOTS_DIR = '/work/ubuntu/wd/base/plots/'
+DATA_DIR = '/work/ubuntu/oggm-data/'
+WORKING_DIR = '/work/ubuntu/wd/'
+PLOTS_DIR = '/work/ubuntu/plots/'
 
-DATA_DIR = '/home/mowglie/disk/Dropbox/Share/oggm-data/'
-WORKING_DIR = '/home/mowglie/disk/ITMIX/mybs100/'
-PLOTS_DIR = '/home/mowglie/disk/ITMIX/plots/'
-ITMIX_ODIR = '/home/mowglie/disk/ITMIX/output/'
+#DATA_DIR = '/home/mowglie/disk/Dropbox/Share/oggm-data/'
+#WORKING_DIR = '/home/mowglie/disk/ITMIX/mybs100/'
+#PLOTS_DIR = '/home/mowglie/disk/ITMIX/plots/'
+ITMIX_ODIR = '/work/ubuntu/output/'
 
 # DATA_DIR = '/home/mowglie/disk/Dropbox/Share/oggm-data/'
 # WORKING_DIR = '/home/mowglie/disk/ITMIX/onefl/'
