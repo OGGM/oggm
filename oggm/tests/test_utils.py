005790aa4687aba3374bf1e46bc119861d65780c
JohannesUIBK <csaq9426@uibk.ac.at>
2016-04-19 14:42:48 +0200

    deleted unnecessary test

--- oggm/tests/test_utils.py
+++ oggm/tests/test_utils.py
@@ -51,19 +51,19 @@ class TestDataFiles(unittest.TestCase):
         self.assertTrue(os.path.exists(lf))
 
 
-    def test_srtmzone(self):
+    def test_srtmfilledzone(self):
 
-        z = utils.srtm_zone(lon_ex=[-112, -112], lat_ex=[57, 57])
+        z = utils.srtm_filled_zone(lon_ex=[-112, -112], lat_ex=[57, 57])
         self.assertTrue(len(z) == 1)
         self.assertEqual('14_01', z[0])
 
-        z = utils.srtm_zone(lon_ex=[-72, -73], lat_ex=[-52, -53])
+        z = utils.srtm_filled_zone(lon_ex=[-72, -73], lat_ex=[-52, -53])
         self.assertTrue(len(z) == 1)
         self.assertEqual('22_23', z[0])
 
         # Alps
         ref = sorted(['39_04', '38_03', '38_04', '39_03'])
-        z = utils.srtm_zone(lon_ex=[6, 14], lat_ex=[41, 48])
+        z = utils.srtm_filled_zone(lon_ex=[6, 14], lat_ex=[41, 48])
         self.assertTrue(len(z) == 4)
         self.assertEqual(ref, z)
 
