ef4fc309feb53e8d584b18610d61312f35f95d0c
Fabien Maussion <fabien.maussion@uibk.ac.at>
2016-08-04 13:41:02 +0200

    added a decorator for plotting maps (#116)

--- oggm/graphics.py
+++ oggm/graphics.py
@@ -5,6 +5,7 @@ from six.moves import zip
 from collections import OrderedDict
 import warnings
 import logging
+import functools
 
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -33,12 +34,86 @@ log = logging.getLogger(__name__)
 
 nullfmt = NullFormatter()  # no labels
 
+
 def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=256):
+    """Remove extreme colors from colormap."""
     new_cmap = colors.LinearSegmentedColormap.from_list(
         'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
         cmap(np.linspace(minval, maxval, n)))
     return new_cmap
 
+
+def _plot_map(plotfunc):
+    """
+    Decorator for common Cleo.Map plotting logic
+    """
+    commondoc = """
+    Parameters
+    ----------
+    ax : matplotlib axes object, optional
+        If None, uses own axis
+    add_colorbar : Boolean, optional, default=True
+        Adds colorbar to axis
+    horizontal_colorbar : Boolean, optional, default=False
+        Horizontal colorbar instead
+    title : str, optional
+        If left to None, the plot decides wether it writes a title or not. Set
+        to '' for no title.
+    title_comment : str, optional
+        add something to the default title. Set to none to remove default
+    lonlat_contours_kwargs: dict, optional
+        pass kwargs to cleo.Map.set_lonlat_contours
+    """
+
+    # Build on the original docstring
+    plotfunc.__doc__ = '\n'.join((plotfunc.__doc__, commondoc))
+
+    @functools.wraps(plotfunc)
+    def newplotfunc(gdir, ax=None, add_colorbar=True, title=None,
+                    title_comment=None, horizontal_colorbar=False,
+                    lonlat_contours_kwargs=None,
+                    **kwargs):
+
+        dofig = False
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            dofig = True
+
+        mp = cleo.Map(gdir.grid, countries=False, nx=gdir.grid.nx)
+        if lonlat_contours_kwargs is not None:
+            mp.set_lonlat_contours(**lonlat_contours_kwargs)
+
+        out = plotfunc(gdir, ax=ax, cleomap=mp, **kwargs)
+
+        if add_colorbar and 'cbar_label' in out:
+            cbprim = out.get('cbar_primitive', mp)
+            if horizontal_colorbar:
+                cb = cbprim.append_colorbar(ax, "bottom", size="5%", pad=0.4)
+            else:
+                cb = cbprim.append_colorbar(ax, "right", size="5%", pad=0.2)
+            cb.set_label(out['cbar_label'])
+
+        if title is None:
+            if 'title' not in out:
+                # Make a defaut one
+                title = gdir.rgi_id
+                if gdir.name is not None and gdir.name != '':
+                    title += ': ' + gdir.name
+                out['title'] = title
+
+            if title_comment is None:
+                title_comment = out.get('title_comment', '')
+
+            out['title'] += title_comment
+            ax.set_title(out['title'])
+        else:
+            ax.set_title(title)
+
+        if dofig:
+            plt.tight_layout()
+
+    return newplotfunc
 
 @entity_task(log)
 def plot_googlemap(gdir, ax=None):
@@ -75,24 +150,20 @@ def plot_googlemap(gdir, ax=None):
 
 
 @entity_task(log)
-def plot_domain(gdir, ax=None):  # pragma: no cover
-    """Plot the glacier directory."""
+@_plot_map
+def plot_domain(gdir, ax=None, cleomap=None):  # pragma: no cover
+    """Plot the glacier directory.
+
+    """
 
     # Files
     with netCDF4.Dataset(gdir.get_filepath('gridded_data')) as nc:
         topo = nc.variables['topo'][:]
 
-    dofig = False
-    if ax is None:
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        dofig = True
-
-    mp = cleo.Map(gdir.grid, countries=False, nx=gdir.grid.nx)
     cm = truncate_colormap(colormap.terrain, minval=0.25, maxval=1.0, n=256)
-    mp.set_cmap(cm)
-    mp.set_plot_params(nlevels=256)
-    mp.set_data(topo)
+    cleomap.set_cmap(cm)
+    cleomap.set_plot_params(nlevels=256)
+    cleomap.set_data(topo)
 
     # TODO: center grid or corner grid???
     crs = gdir.grid.center_grid
@@ -103,28 +174,24 @@ def plot_domain(gdir, ax=None):  # pragma: no cover
         # Plot boundaries
         poly_pix = geom['polygon_pix']
 
-        mp.set_geometry(poly_pix, crs=crs, fc='white',
+        cleomap.set_geometry(poly_pix, crs=crs, fc='white',
                          alpha=0.3, zorder=2, linewidth=.2)
         for l in poly_pix.interiors:
-            mp.set_geometry(l, crs=crs,
+            cleomap.set_geometry(l, crs=crs,
                               color='black', linewidth=0.5)
 
-    mp.plot(ax)
-    cb = mp.append_colorbar(ax, "right", size="5%", pad=0.2)
-    cb.set_label('Alt. [m]')
-    title = gdir.rgi_id
-    if gdir.name is not None and gdir.name != '':
-        title += ': ' + gdir.name
-    ax.set_title(title)
+    cleomap.plot(ax)
 
-    if dofig:
-        plt.tight_layout()
+    return dict(cbar_label='Alt. [m]')
 
 
 @entity_task(log)
-def plot_centerlines(gdir, ax=None, use_flowlines=False,
+@_plot_map
+def plot_centerlines(gdir, ax=None, cleomap=None, use_flowlines=False,
                      add_downstream=False):
-    """Plots the centerlines of a glacier directory."""
+    """Plots the centerlines of a glacier directory.
+
+    """
 
     # Files
     filename = 'centerlines'
@@ -134,17 +201,10 @@ def plot_centerlines(gdir, ax=None, use_flowlines=False,
     with netCDF4.Dataset(gdir.get_filepath('gridded_data')) as nc:
         topo = nc.variables['topo'][:]
 
-    dofig = False
-    if ax is None:
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        dofig = True
-
-    mp = cleo.Map(gdir.grid, countries=False, nx=gdir.grid.nx)
     cm = truncate_colormap(colormap.terrain, minval=0.25, maxval=1.0, n=256)
-    mp.set_cmap(cm)
-    mp.set_plot_params(nlevels=256)
-    mp.set_data(topo)
+    cleomap.set_cmap(cm)
+    cleomap.set_plot_params(nlevels=256)
+    cleomap.set_data(topo)
 
     # TODO: center grid or corner grid???
     crs = gdir.grid.center_grid
@@ -155,65 +215,54 @@ def plot_centerlines(gdir, ax=None, use_flowlines=False,
         # Plot boundaries
         poly_pix = geom['polygon_pix']
 
-        mp.set_geometry(poly_pix, crs=crs, fc='white',
-                         alpha=0.3, zorder=2, linewidth=.2)
+        cleomap.set_geometry(poly_pix, crs=crs, fc='white',
+                             alpha=0.3, zorder=2, linewidth=.2)
         for l in poly_pix.interiors:
-            mp.set_geometry(l, crs=crs,
-                              color='black', linewidth=0.5)
+            cleomap.set_geometry(l, crs=crs,
+                                 color='black', linewidth=0.5)
 
         # plot Centerlines
         cls = gdir.read_pickle(filename, div_id=i)
 
         # Go in reverse order for red always being the longuest
         cls = cls[::-1]
-        color = gpd.plotting.gencolor(len(cls)+1, colormap='Set1')
+        color = gpd.plotting.gencolor(len(cls) + 1, colormap='Set1')
         for l, c in zip(cls, color):
-            mp.set_geometry(l.line, crs=crs, color=c,
-                              linewidth=2.5, zorder=50)
-            mp.set_geometry(l.head, crs=gdir.grid, marker='o',
-                            markersize=60, alpha=0.8, color=c, zorder=99)
+            cleomap.set_geometry(l.line, crs=crs, color=c,
+                                 linewidth=2.5, zorder=50)
+            cleomap.set_geometry(l.head, crs=gdir.grid, marker='o',
+                                 markersize=60, alpha=0.8, color=c, zorder=99)
 
             for j in l.inflow_points:
-                mp.set_geometry(j, crs=crs, marker='o',
-                                  markersize=40, edgecolor='k', alpha=0.8,
-                                  zorder=99, facecolor='none')
+                cleomap.set_geometry(j, crs=crs, marker='o',
+                                     markersize=40, edgecolor='k', alpha=0.8,
+                                     zorder=99, facecolor='none')
 
         if add_downstream:
             line = gdir.read_pickle('downstream_line', div_id=i)
-            mp.set_geometry(line, crs=crs, color='red', linewidth=2.5,
-                              zorder=50)
+            cleomap.set_geometry(line, crs=crs, color='red', linewidth=2.5,
+                                 zorder=50)
 
-            mp.set_geometry(shpg.Point(line.coords[0]), crs=crs, marker='o',
-                                  markersize=40, edgecolor='k', alpha=0.8,
-                                  zorder=99, facecolor='w')
+            cleomap.set_geometry(shpg.Point(line.coords[0]), crs=crs,
+                                 marker='o', markersize=40, edgecolor='k',
+                                 alpha=0.8, zorder=99, facecolor='w')
 
-    mp.plot(ax)
-    cb = mp.append_colorbar(ax, "right", size="5%", pad=0.2)
-    cb.set_label('Alt. [m]')
-    title = gdir.rgi_id
-    if gdir.name is not None and gdir.name != '':
-        title += ': ' + gdir.name
-    ax.set_title(title)
+    cleomap.plot(ax)
 
-    if dofig:
-        plt.tight_layout()
+    return dict(cbar_label='Alt. [m]')
 
 
 @entity_task(log)
-def plot_catchment_width(gdir, ax=None, corrected=False):
-    """Plots the catchment widths out of a glacier directory."""
+@_plot_map
+def plot_catchment_width(gdir, ax=None, cleomap=None, corrected=False):
+    """Plots the catchment widths out of a glacier directory.
 
-    dofig = False
-    if ax is None:
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        dofig = True
+    """
 
     with netCDF4.Dataset(gdir.get_filepath('gridded_data')) as nc:
         topo = nc.variables['topo'][:]
 
-    mp = cleo.Map(gdir.grid, countries=False, nx=gdir.grid.nx)
-    mp.set_topography(topo)
+    cleomap.set_topography(topo)
 
     # TODO: center grid or corner grid???
     crs = gdir.grid.center_grid
@@ -222,51 +271,46 @@ def plot_catchment_width(gdir, ax=None, corrected=False):
 
         # Plot boundaries
         poly_pix = geom['polygon_pix']
-        mp.set_geometry(poly_pix, crs=crs, fc='none', zorder=2, linewidth=.2)
+        cleomap.set_geometry(poly_pix, crs=crs, fc='none', zorder=2,
+                             linewidth=.2)
         for l in poly_pix.interiors:
-            mp.set_geometry(l, crs=crs, color='black', linewidth=0.5)
+            cleomap.set_geometry(l, crs=crs, color='black', linewidth=0.5)
 
         # plot Centerlines
         cls = gdir.read_pickle('inversion_flowlines', div_id=i)[::-1]
-        color = gpd.plotting.gencolor(len(cls)+1, colormap='Set1')
+        color = gpd.plotting.gencolor(len(cls) + 1, colormap='Set1')
         for l, c in zip(cls, color):
-            mp.set_geometry(l.line, crs=crs, color=c,
-                              linewidth=2.5, zorder=50)
+            cleomap.set_geometry(l.line, crs=crs, color=c,
+                                 linewidth=2.5, zorder=50)
             if corrected:
                 for wi, cur, (n1, n2) in zip(l.widths, l.line.coords,
                                              l.normals):
-                    l = shpg.LineString([shpg.Point(cur + wi/2. * n1),
-                                         shpg.Point(cur + wi/2. * n2)])
+                    l = shpg.LineString([shpg.Point(cur + wi / 2. * n1),
+                                         shpg.Point(cur + wi / 2. * n2)])
 
-                    mp.set_geometry(l, crs=crs, color=c,
-                                      linewidth=0.6, zorder=50)
+                    cleomap.set_geometry(l, crs=crs, color=c,
+                                         linewidth=0.6, zorder=50)
             else:
                 for wl, wi in zip(l.geometrical_widths, l.widths):
                     col = c if np.isfinite(wi) else 'grey'
                     for w in wl:
-                        mp.set_geometry(w, crs=crs, color=col,
-                                          linewidth=0.6, zorder=50)
+                        cleomap.set_geometry(w, crs=crs, color=col,
+                                             linewidth=0.6, zorder=50)
 
-    mp.plot(ax)
-    if dofig:
-        plt.tight_layout()
+    cleomap.plot(ax)
+
+    return {}
 
 
 @entity_task(log)
-def plot_inversion(gdir, ax=None, add_title_comment=''):
+@_plot_map
+def plot_inversion(gdir, ax=None, cleomap=None):
     """Plots the result of the inversion out of a glacier directory."""
-
-    dofig = False
-    if ax is None:
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        dofig = True
 
     with netCDF4.Dataset(gdir.get_filepath('gridded_data')) as nc:
         topo = nc.variables['topo'][:]
 
-    mp = cleo.Map(gdir.grid, countries=False, nx=gdir.grid.nx)
-    mp.set_topography(topo)
+    cleomap.set_topography(topo)
 
     # TODO: center grid or corner grid???
     crs = gdir.grid.center_grid
@@ -279,20 +323,21 @@ def plot_inversion(gdir, ax=None, add_title_comment=''):
         inv = gdir.read_pickle('inversion_output', div_id=i)
         # Plot boundaries
         poly_pix = geom['polygon_pix']
-        mp.set_geometry(poly_pix, crs=crs, fc='none', zorder=2, linewidth=.2)
+        cleomap.set_geometry(poly_pix, crs=crs, fc='none', zorder=2,
+                             linewidth=.2)
         for l in poly_pix.interiors:
-            mp.set_geometry(l, crs=crs, color='black', linewidth=0.5)
+            cleomap.set_geometry(l, crs=crs, color='black', linewidth=0.5)
 
         # plot Centerlines
         cls = gdir.read_pickle('inversion_flowlines', div_id=i)
         for l, c in zip(cls, inv):
 
-            mp.set_geometry(l.line, crs=crs, color='gray',
-                              linewidth=1.2, zorder=50)
+            cleomap.set_geometry(l.line, crs=crs, color='gray',
+                                 linewidth=1.2, zorder=50)
             toplot_th = np.append(toplot_th, c['thick'])
             for wi, cur, (n1, n2) in zip(l.widths, l.line.coords, l.normals):
-                l = shpg.LineString([shpg.Point(cur + wi/2. * n1),
-                                     shpg.Point(cur + wi/2. * n2)])
+                l = shpg.LineString([shpg.Point(cur + wi / 2. * n1),
+                                     shpg.Point(cur + wi / 2. * n2)])
                 toplot_lines.append(l)
             vol.extend(c['volume'])
 
@@ -300,33 +345,22 @@ def plot_inversion(gdir, ax=None, add_title_comment=''):
     dl = cleo.DataLevels(cmap=cm, nlevels=256, data=toplot_th, vmin=0)
     colors = dl.to_rgb()
     for l, c in zip(toplot_lines, colors):
-        mp.set_geometry(l, crs=crs, color=c,
-                          linewidth=3, zorder=50)
-    mp.plot(ax)
-    cb = dl.append_colorbar(ax, "right", size="5%", pad=0.2)
-    cb.set_label('Glacier thickness [m]')
-    title = gdir.rgi_id
-    if gdir.name is not None and gdir.name != '':
-        title += ': ' + gdir.name
-    title += add_title_comment
-    title += ' ({:.2f} km3)'.format(np.nansum(vol) * 1e-9)
-    ax.set_title(title)
-    if dofig:
-        plt.tight_layout()
+        cleomap.set_geometry(l, crs=crs, color=c,
+                             linewidth=3, zorder=50)
+    cleomap.plot(ax)
+
+    return dict(cbar_label='Section thickness [m]',
+                cbar_primitive=dl,
+                title_comment=' ({:.2f} km3)'.format(np.nansum(vol) * 1e-9))
 
 
 @entity_task(log)
-def plot_distributed_thickness(gdir, ax=None, how=None):
+@_plot_map
+def plot_distributed_thickness(gdir, ax=None, cleomap=None, how=None):
     """Plots the result of the inversion out of a glacier directory.
 
     Method: 'alt' or 'interp'
     """
-
-    dofig = False
-    if ax is None:
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        dofig = True
 
     with netCDF4.Dataset(gdir.get_filepath('gridded_data')) as nc:
         topo = nc.variables['topo'][:]
@@ -341,48 +375,33 @@ def plot_distributed_thickness(gdir, ax=None, how=None):
 
     thick = np.where(mask, thick, np.NaN)
 
-    mp = cleo.Map(gdir.grid, countries=False, nx=gdir.grid.nx)
-    mp.set_topography(topo)
+    cleomap.set_topography(topo)
 
     # TODO: center grid or corner grid???
     crs = gdir.grid.center_grid
 
-    toplot_th = np.array([])
-    toplot_lines = []
     for i in gdir.divide_ids:
         geom = gdir.read_pickle('geometries', div_id=i)
 
         # Plot boundaries
         poly_pix = geom['polygon_pix']
-        mp.set_geometry(poly_pix, crs=crs, fc='none', zorder=2, linewidth=.2)
+        cleomap.set_geometry(poly_pix, crs=crs, fc='none', zorder=2, linewidth=.2)
         for l in poly_pix.interiors:
-            mp.set_geometry(l, crs=crs, color='black', linewidth=0.5)
+            cleomap.set_geometry(l, crs=crs, color='black', linewidth=0.5)
 
-    mp.set_cmap(plt.get_cmap('viridis'))
-    mp.set_plot_params(nlevels=256)
-    mp.set_data(thick)
+    cleomap.set_cmap(plt.get_cmap('viridis'))
+    cleomap.set_plot_params(nlevels=256)
+    cleomap.set_data(thick)
 
-    mp.plot(ax)
-    cb = mp.append_colorbar(ax, "right", size="5%", pad=0.2)
-    cb.set_label('Thick. [m]')
-    title = gdir.rgi_id
-    if gdir.name is not None and gdir.name != '':
-        title += ': ' + gdir.name
-    ax.set_title(title)
+    cleomap.plot(ax)
 
-    if dofig:
-        plt.tight_layout()
+    return dict(cbar_label='Glacier thickness [m]')
 
 
 @entity_task(log)
-def plot_modeloutput_map(gdir, model=None, ax=None, vmax=None):
+@_plot_map
+def plot_modeloutput_map(gdir, ax=None, cleomap=None, model=None, vmax=None):
     """Plots the result of the model output."""
-
-    dofig = False
-    if ax is None:
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        dofig = True
 
     with netCDF4.Dataset(gdir.get_filepath('gridded_data')) as nc:
         topo = nc.variables['topo'][:]
@@ -399,14 +418,14 @@ def plot_modeloutput_map(gdir, model=None, ax=None, vmax=None):
         warnings.filterwarnings("ignore",category=RuntimeWarning)
         ds.set_subset(corners=corners, margin=10, crs=gdir.grid)
 
-    mp = cleo.Map(ds.grid, countries=False, nx=gdir.grid.nx)
-    mp.set_topography(topo, crs=gdir.grid)
+    cleomap = cleo.Map(ds.grid, countries=False, nx=gdir.grid.nx)
+    cleomap.set_topography(topo, crs=gdir.grid)
 
     # TODO: center grid or corner grid???
     crs = gdir.grid.center_grid
-    mp.set_geometry(poly_pix, crs=crs, fc='none', zorder=2, linewidth=.2)
+    cleomap.set_geometry(poly_pix, crs=crs, fc='none', zorder=2, linewidth=.2)
     for l in poly_pix.interiors:
-        mp.set_geometry(l, crs=crs, color='black', linewidth=0.5)
+        cleomap.set_geometry(l, crs=crs, color='black', linewidth=0.5)
 
     toplot_th = np.array([])
     toplot_lines = []
@@ -414,7 +433,7 @@ def plot_modeloutput_map(gdir, model=None, ax=None, vmax=None):
     # plot Centerlines
     cls = model.fls
     for l in cls:
-        mp.set_geometry(l.line, crs=crs, color='gray',
+        cleomap.set_geometry(l.line, crs=crs, color='gray',
                           linewidth=1.2, zorder=50)
         toplot_th = np.append(toplot_th, l.thick)
         widths = l.widths.copy()
@@ -428,16 +447,13 @@ def plot_modeloutput_map(gdir, model=None, ax=None, vmax=None):
     dl = cleo.DataLevels(cmap=cm, nlevels=256, data=toplot_th, vmin=0, vmax=vmax)
     colors = dl.to_rgb()
     for l, c in zip(toplot_lines, colors):
-        mp.set_geometry(l, crs=crs, color=c,
+        cleomap.set_geometry(l, crs=crs, color=c,
                           linewidth=3, zorder=50)
-    mp.plot(ax)
-    cb = dl.append_colorbar(ax, "right", size="5%", pad=0.2)
-    cb.set_label('Glacier thickness [m]')
-    tit = ' -- year: {:d}'.format(np.int64(model.yr))
-    ax.set_title(gdir.rgi_id + tit)
+    cleomap.plot(ax)
 
-    if dofig:
-        plt.tight_layout()
+    return dict(cbar_label='Section thickness [m]',
+                cbar_primitive=dl,
+                title_comment=' -- year: {:d}'.format(np.int64(model.yr)))
 
 
 @entity_task(log)
