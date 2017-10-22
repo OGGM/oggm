c9097cf1aa8d980001015877d06788a7ba63b7f1
Fabien Maussion <fabien.maussion@uibk.ac.at>
2017-10-22 13:21:04 +0200

    Updates in docs

--- oggm/sandbox/gmd_paper/plot_workflow.py
+++ oggm/sandbox/gmd_paper/plot_workflow.py
@@ -1,6 +1,8 @@
 import os
+import shutil
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import scipy.optimize as optimization
 
 import oggm
 from oggm import cfg, tasks, graphics
@@ -8,16 +10,19 @@ from oggm.sandbox.gmd_paper import PLOT_DIR
 from oggm import utils
 
 cfg.initialize()
-cfg.PARAMS['border'] = 15
+cfg.PARAMS['grid_dx_method'] = 'fixed'
+cfg.PARAMS['fixed_dx'] = 100
+cfg.PARAMS['border'] = 20
 cfg.PARAMS['auto_skip_task'] = True
-reset = False
 
 base_dir = os.path.join(os.path.expanduser('~/tmp'), 'OGGM_GMD', 'Workflow')
 cfg.PATHS['working_dir'] = base_dir
-utils.mkdir(base_dir, reset=reset)
 
-entity = gpd.read_file(utils.get_demo_file('HEF_MajDivide.shp')).iloc[0]
+mbf = 'https://dl.dropboxusercontent.com/u/20930277/ref_tstars_no_tidewater.csv'
+mbf = utils.file_downloader(mbf)
+shutil.copyfile(mbf, os.path.join(base_dir, 'ref_tstars.csv'))
 
+entity = gpd.read_file('/home/mowglie/Documents/OGGM/Tasman/tasman.shp').iloc[0]
 gdir = oggm.GlacierDirectory(entity, base_dir=base_dir)
 
 tasks.define_glacier_region(gdir, entity=entity)
@@ -31,19 +36,18 @@ tasks.catchment_intersections(gdir)
 tasks.catchment_width_geom(gdir)
 tasks.catchment_width_correction(gdir)
 tasks.process_cru_data(gdir)
-tasks.compute_ref_t_stars([gdir])
 tasks.distribute_t_stars([gdir])
 
-glen_a = cfg.A * 1.5
-
+glen_a = cfg.A
 tasks.prepare_for_inversion(gdir)
 tasks.volume_inversion(gdir, use_cfg_params={'fs':0, 'glen_a':glen_a})
 tasks.filter_inversion_output(gdir)
 
 # run
 tasks.init_present_time_glacier(gdir)
-tasks.random_glacier_evolution(gdir, bias=0., glen_a=glen_a, nyears=200,
-                               seed=1)
+tasks.random_glacier_evolution(gdir, bias=0., temperature_bias=-1,
+                               nyears=120, glen_a=glen_a,
+                               check_for_boundaries=False)
 
 # ds = utils.compile_run_output([gdir], path=False)
 # ds.volume.plot();
@@ -71,7 +75,7 @@ letkm = dict(color='black', ha='left', va='top', fontsize=20,
 
 graphics.plot_domain(gdir, ax=axs[0], title='', add_colorbar=False,
                      lonlat_contours_kwargs=llkw)
-xt, yt = 2.45, 2.45
+xt, yt = 4.5, 4.5
 axs[0].text(xt, yt, 'a', **letkm)
 
 im = graphics.plot_centerlines(gdir, ax=axs[1], title='', add_colorbar=True,
@@ -92,20 +96,18 @@ axs[3].text(xt, yt, 'd', **letkm)
 
 f.delaxes(axs[3].cax)
 
-graphics.plot_inversion(gdir, ax=axs[4], title='', linewidth=2,
-                        add_colorbar=False,
-                        vmax=250,
+graphics.plot_inversion(gdir, ax=axs[4], title='', linewidth=1.5,
+                        add_colorbar=False, vmax=650,
                         lonlat_contours_kwargs=llkw,
                         add_scalebar=False)
 axs[4].text(xt, yt, 'e', **letkm)
 
-graphics.plot_modeloutput_map(gdir, ax=axs[5], modelyr=175, title='',
-                              linewidth=2, add_colorbar=True,
-                              vmax=250,
-                              cbar_ax=axs[5].cax,
+graphics.plot_modeloutput_map(gdir, ax=axs[5], modelyr=120, title='',
+                              linewidth=1.5, add_colorbar=True,
+                              cbar_ax=axs[5].cax, vmax=650,
                               lonlat_contours_kwargs=llkw,
                               add_scalebar=False)
 axs[5].text(xt, yt, 'f', **letkm)
 
 # plt.tight_layout()
-plt.savefig(PLOT_DIR + 'workflow.pdf', dpi=150, bbox_inches='tight')
+plt.savefig(PLOT_DIR + 'workflow_tas.pdf', dpi=150, bbox_inches='tight')
