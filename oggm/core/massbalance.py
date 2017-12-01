061aa11d50c92026732544347f2fcc42fccec02b
David <sumnonpuella@gmail.com>
2017-12-01 10:38:18 -0600

    corrected kwargs in ConstantMassBalance

--- oggm/core/massbalance.py
+++ oggm/core/massbalance.py
@@ -354,7 +354,7 @@ class ConstantMassBalance(MassBalanceModel):
 
         super(ConstantMassBalance, self).__init__()
         self.mbmod = PastMassBalance(gdir, mu_star=mu_star, bias=bias,
-                                     prcp_fac=prcp_fac, filename='climate_monthly', input_filesuffix='')
+                                     prcp_fac=prcp_fac, filename=filename, input_filesuffix='')
 
         if y0 is None:
             df = pd.read_csv(gdir.get_filepath('local_mustar'))
