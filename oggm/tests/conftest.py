3266ecee8f9a6601d8678a91e8923c6d7137adb3
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-05-24 09:34:31 +0200

    Make ilock lock name user specific

--- oggm/tests/conftest.py
+++ oggm/tests/conftest.py
@@ -1,8 +1,7 @@
 import pytest
 import logging
-import multiprocessing as mp
+import getpass
 from oggm import cfg, utils
-import pickle
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +9,7 @@ def pytest_configure(config):
     if config.pluginmanager.hasplugin('xdist'):
         try:
             from ilock import ILock
-            utils.lock = ILock("oggm_xdist_download_lock")
+            utils.lock = ILock("oggm_xdist_download_lock_" + getpass.getuser())
             logger.info("ilock locking setup successfully for xdist tests")
         except:
             logger.warning("could not setup ilock locking for distributed tests")
