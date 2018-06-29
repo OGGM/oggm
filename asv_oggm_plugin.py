85a929ca9832ebb66120065c8dd0affa2d7e8c6d
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-06-29 23:24:42 +0200

    Environment variables cannot contain dots.

--- asv_oggm_plugin.py
+++ asv_oggm_plugin.py
@@ -13,7 +13,7 @@ class OggmVirtualenv(Virtualenv):
         # pip>=10 is broken for some of our dependencies
         self.run_executable("pip", ["install", "-v", "--upgrade", "pip<10", "setuptools"])
 
-        env_key = "OGGM_ASV_WHEEL_DIR_" + self._python
+        env_key = "OGGM_ASV_WHEEL_DIR_" + self._python.replace(".", "_")
         if env_key not in os.environ:
              env_key = "OGGM_ASV_WHEEL_DIR"
 
