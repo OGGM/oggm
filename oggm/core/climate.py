bcb8a0c6fe64b158e1d1c1bdba02009965f4fffb
oberrauch <oberrauch.moritz@gmail.com>
2018-11-27 16:08:58 +0100

    Corrected some documentation typos

--- oggm/core/climate.py
+++ oggm/core/climate.py
@@ -575,7 +575,7 @@ def mb_climate_on_height(gdir, heights, *, time_range=None, year_range=None):
         can provide a [t0, t1] bounds (inclusive).
     year_range : [int, int], optional
         Provide a [y0, y1] year range to get the data for specific
-        (hydrological) years only. Easier to us than the time bounds above.
+        (hydrological) years only. Easier to use than the time bounds above.
 
     Returns
     -------
@@ -734,9 +734,9 @@ def mb_yearly_climate_on_glacier(gdir, *, year_range=None):
     Returns
     -------
     (years, tempformelt, prcpsol)::
-        - years: array of shape (ny,)
-        - tempformelt:  array of shape (len(heights), ny)
-        - prcpsol:  array of shape (len(heights), ny) (not corrected!)
+        - years: array of shape (ny)
+        - tempformelt:  array of shape (ny)
+        - prcpsol:  array of shape (ny) (not corrected!)
     """
 
     flowlines = gdir.read_pickle('inversion_flowlines')
@@ -942,7 +942,7 @@ def calving_mb(gdir):
     if not gdir.is_tidewater:
         return 0.
 
-    # Ok. Just take the caving rate from cfg and change its units
+    # Ok. Just take the calving rate from cfg and change its units
     # Original units: km3 a-1, to change to mm a-1 (units of specific MB)
     rho = cfg.PARAMS['ice_density']
     return gdir.inversion_calving_rate * 1e9 * rho / gdir.rgi_area_m2
@@ -994,7 +994,7 @@ def local_t_star(gdir, *, ref_df=None, tstar=None, bias=None):
                     if cfg.PARAMS[k] != cfg.PARAMS[vn][k]:
                         raise ValueError('The reference t* you are trying '
                                          'to use was calibrated with '
-                                         'difference MB parameters. You '
+                                         'different MB parameters. You '
                                          'might have to run the calibration '
                                          'manually.')
                 ref_df = cfg.PARAMS['ref_tstars_rgi{}_{}'.format(v, str_s)]
@@ -1160,6 +1160,7 @@ def mu_star_calibration(gdir):
 
     Parameters
     ----------
+    gdir : oggm.GlacierDirectory
     """
 
     # Interpolated data
