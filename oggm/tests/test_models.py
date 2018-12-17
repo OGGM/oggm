c935e32779b8f54b7c702213e4430f89b15db4d8
Matthias Dusch <matthias.dusch@uibk.ac.at>
2018-12-17 18:55:14 +0100

    changed test-function to not download rgi-files

--- oggm/tests/test_models.py
+++ oggm/tests/test_models.py
@@ -2726,12 +2726,12 @@ class TestMergedHEF(unittest.TestCase):
         id = [gd.rgi_id == 'RGI50-11.00897' for gd in gdirs]
         hef_gdir = gdirs.pop(np.where(id)[0][0])
 
-        # list of possible tributaries:
-        tributaries = ['RGI60-11.00787',  # 'Kesselwandferner',
-                       'RGI60-11.00746']  # 'Gepatschferner',
+        # KWF and Gepatsch as possible tributaries
+        trbdf = rgidf.loc[(rgidf.RGIId == 'RGI50-11.00746') |
+                          (rgidf.RGIId == 'RGI50-11.00787')].copy()
 
         # merge HEF and KWF
-        gdir_merged = workflow.merge_glacier_tasks(hef_gdir, tributaries)
+        gdir_merged = workflow.merge_glacier_tasks(hef_gdir, trbdf)
 
         # and run the merged glacier
         workflow.execute_entity_task(tasks.run_constant_climate,
