b9278732138dba000c0131255c9ab1ae1d00fad8
Fabien Maussion <fabien.maussion@uibk.ac.at>
2019-01-02 18:18:03 +0100

    Do not copy gridded data for minimal gdir setup

--- oggm/utils/_workflow.py
+++ oggm/utils/_workflow.py
@@ -2032,7 +2032,7 @@ def copy_to_basedir(gdir, base_dir=None, setup='run'):
                            gdir.rgi_id)
     if setup == 'run':
         paths = ['model_flowlines', 'inversion_params', 'outlines',
-                 'local_mustar', 'climate_monthly', 'gridded_data',
+                 'local_mustar', 'climate_monthly',
                  'gcm_data', 'climate_info']
         paths = ('*' + p + '*' for p in paths)
         shutil.copytree(gdir.dir, new_dir,
