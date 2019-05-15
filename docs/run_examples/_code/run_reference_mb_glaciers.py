bc0ba2394a04cdba00a17e246a16aa05a2e20108
oberrauch <oberrauch.moritz@gmail.com>
2019-05-15 14:19:48 +0200

    Selecting only glaciers in the Alp for HISTALP mass balance calibration.

--- docs/run_examples/_code/run_reference_mb_glaciers.py
+++ docs/run_examples/_code/run_reference_mb_glaciers.py
@@ -77,9 +77,12 @@ log.info('Process the climate data...')
 if baseline == 'CRU':
     execute_entity_task(tasks.process_cru_data, gdirs, print_log=False)
 elif baseline == 'HISTALP':
+	# exclude glaciers outside of the Alps
+	gdirs = [gdir for gdir in gdirs if gdir.rgi_subregion == '11-01']
     cfg.PARAMS['continue_on_error'] = True  # Some glaciers are not in Alps
     execute_entity_task(tasks.process_histalp_data, gdirs, print_log=False)
     cfg.PARAMS['continue_on_error'] = False
+
 
 gdirs = utils.get_ref_mb_glaciers(gdirs)
 
