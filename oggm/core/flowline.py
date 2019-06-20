51f35606e25fcf2bacc8bdf3517df2ff4b324fd1
juliaeis <jeis@uni-bremen.de>
2019-06-20 15:00:20 +0200

    add bias option for run_from_climate_data()

--- oggm/core/flowline.py
+++ oggm/core/flowline.py
@@ -1880,7 +1880,7 @@ def run_from_climate_data(gdir, ys=None, ye=None, min_ys=None,
                           climate_filename='climate_monthly',
                           climate_input_filesuffix='', output_filesuffix='',
                           init_model_filesuffix=None, init_model_yr=None,
-                          init_model_fls=None, zero_initial_glacier=False,
+                          init_model_fls=None, zero_initial_glacier=False,bias=None,
                           **kwargs):
     """ Runs a glacier with climate input from e.g. CRU or a GCM.
 
@@ -1945,7 +1945,7 @@ def run_from_climate_data(gdir, ys=None, ye=None, min_ys=None,
             init_model_fls = fmod.fls
 
     mb = MultipleFlowlineMassBalance(gdir, mb_model_class=PastMassBalance,
-                                     filename=climate_filename,
+                                     filename=climate_filename, bias=bias,
                                      input_filesuffix=climate_input_filesuffix)
 
     return robust_model_run(gdir, output_filesuffix=output_filesuffix,
