927466c2a91058519785540d3d67af89bb623bdc
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2019-10-03 13:30:30 +0200

    Switch to Python 3.7 as main test environment

--- ci/mk_travis_yaml.sh
+++ ci/mk_travis_yaml.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MAIN_TEST_CONTAINER_TAG="20181123"
+MAIN_TEST_CONTAINER_TAG="20191004"
 
 for os in linux; do
-	for test_container in $MAIN_TEST_CONTAINER_TAG py37; do
+	for test_container in $MAIN_TEST_CONTAINER_TAG py36; do
 		for test_env in prepro numerics models benchmark utils workflow graphics; do
 			EXT="TEST_CONTAINER=$test_container OGGM_TEST_ENV=$test_env"
 			if [[ "$test_env" == "workflow" ]] || [[ "$test_env" == "graphics" ]]; then
