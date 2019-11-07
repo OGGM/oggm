b54c9e2426e36b76cc4e7ede022e36bffba3addf
Fabien Maussion <fabien.maussion@uibk.ac.at>
2019-11-07 17:52:22 +0100

    forgot self-install (#898)

--- docs/_code/prepare_flowlines.py
+++ docs/_code/prepare_flowlines.py
@@ -8,8 +8,9 @@ cfg.set_intersects_db(get_demo_file('rgi_intersect_oetztal.shp'))
 cfg.PATHS['dem_file'] = get_demo_file('hef_srtm.tif')
 
 base_dir = gettempdir('Flowlines_Docs')
+cfg.PATHS['working_dir'] = base_dir
 entity = gpd.read_file(get_demo_file('HEF_MajDivide.shp')).iloc[0]
-gdir = oggm.GlacierDirectory(entity, base_dir=base_dir)
+gdir = oggm.GlacierDirectory(entity, base_dir=base_dir, reset=True)
 
 tasks.define_glacier_region(gdir, entity=entity)
 tasks.glacier_masks(gdir)
