54faf32227f01ffbcb826e73de9948933fc5edf9
Fabien Maussion <fabien.maussion@uibk.ac.at>
2020-05-14 11:04:50 +0200

    add area-wide checks

--- oggm/tests/test_workflow.py
+++ oggm/tests/test_workflow.py
@@ -236,7 +236,11 @@ class TestFullRun(unittest.TestCase):
             df.loc[gd.rgi_id, 'start_volume_km3'] = model.volume_km3
             df.loc[gd.rgi_id, 'start_length'] = model.length_m
         assert_allclose(df['rgi_area_km2'], df['start_area_km2'], 0.06)
+        assert_allclose(df['rgi_area_km2'].sum(), df['start_area_km2'].sum(),
+                        0.003)
         assert_allclose(df['inv_volume_km3'], df['start_volume_km3'], 0.04)
+        assert_allclose(df['inv_volume_km3'].sum(),
+                        df['start_volume_km3'].sum(), 0.001)
         assert_allclose(df['main_flowline_length'], df['start_length'])
 
         workflow.execute_entity_task(flowline.run_random_climate, gdirs,
