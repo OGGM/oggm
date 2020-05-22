a90a0dfa81a91468d13743c426e9496320410beb
Fabien Maussion <fabien.maussion@uibk.ac.at>
2020-05-22 13:06:23 +0200

    fix test

--- oggm/tests/test_utils.py
+++ oggm/tests/test_utils.py
@@ -1166,9 +1166,13 @@ class TestFakeDownloads(unittest.TestCase):
         utils.mkdir(cfg.PATHS['tmp_dir'])
         utils.mkdir(cfg.PATHS['rgi_dir'])
 
-    def prepare_verify_test(self, valid_size=True, valid_crc32=True):
+    def prepare_verify_test(self, valid_size=True, valid_crc32=True,
+                            reset_dl_dict=True):
         self.reset_dir()
         cfg.PARAMS['dl_verify'] = True
+
+        if reset_dl_dict:
+            cfg.DL_VERIFIED.clear()
 
         tgt_path = os.path.join(cfg.PATHS['dl_cache_dir'], 'test.com',
                                 'test.txt')
@@ -1216,7 +1220,6 @@ class TestFakeDownloads(unittest.TestCase):
             url = self.prepare_verify_test(False, False)
             with self.assertRaises(DownloadVerificationFailedException):
                 utils.oggm_urlretrieve(url)
-
 
     def test_github_no_internet(self):
         self.reset_dir()
