58652b9ea71d0e57dfb96926ed63442e1f6d4b05
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2020-07-20 15:36:28 +0200

    Get rid of old and long defunct freetype_old baseline images

--- oggm/tests/__init__.py
+++ oggm/tests/__init__.py
@@ -16,12 +16,7 @@ if LooseVersion(matplotlib.__version__) >= LooseVersion('2'):
     HAS_MPL_FOR_TESTS = True
     BASELINE_DIR = os.path.join(cfg.CACHE_DIR,
                                 'oggm-sample-data-%s' % SAMPLE_DATA_COMMIT,
-                                'baseline_images')
-    ftver = LooseVersion(matplotlib.ft2font.__freetype_version__)
-    if ftver >= LooseVersion('2.8.0'):
-        BASELINE_DIR = os.path.join(BASELINE_DIR, 'freetype_28')
-    else:
-        BASELINE_DIR = os.path.join(BASELINE_DIR, 'freetype_old')
+                                'baseline_images', 'freetype_28')
 
 # quick n dirty method to see if internet is on
 try:
