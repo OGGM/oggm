2c128ca520b5fa8adb169677bb2f0274cee4116b
Fabien Maussion <fabien.maussion@uibk.ac.at>
2020-07-26 15:46:57 +0200

    Fix more tests

--- oggm/utils/_downloads.py
+++ oggm/utils/_downloads.py
@@ -71,7 +71,7 @@ logger = logging.getLogger('.'.join(__name__.split('.')[:-1]))
 # The given commit will be downloaded from github and used as source for
 # all sample data
 SAMPLE_DATA_GH_REPO = 'OGGM/oggm-sample-data'
-SAMPLE_DATA_COMMIT = 'b0bd2f361ce305b7b9d2267abb405e1bd0cf3f4e'
+SAMPLE_DATA_COMMIT = '9eb6a1c0b00ef666f0ca3719ad40fcc33d982f9b'
 
 GDIR_URL = 'https://cluster.klima.uni-bremen.de/~oggm/gdirs/oggm_v1.1/'
 DEMO_GDIR_URL = 'https://cluster.klima.uni-bremen.de/~oggm/demo_gdirs/'
