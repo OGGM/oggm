b3476bfe72fe3d00d45b36366ff4123c0f1353e3
Keeptg <lifei15@outlook.com>
2020-12-18 17:34:54 +0100

    Add tests for gridded_data_var_to_geotiff()

--- oggm/tests/test_prepro.py
+++ oggm/tests/test_prepro.py
@@ -481,6 +481,22 @@ class TestGIS(unittest.TestCase):
         gdir.write_pickle(out, 'mybn')
         assert gdir.read_pickle('mybn') == out
 
+    def test_gridded_data_var_to_geotiff(self):
+
+        hef_file = get_demo_file('Hintereisferner_RGI5.shp')
+        entity = gpd.read_file(hef_file).iloc[0]
+        gdir = oggm.GlacierDirectory(entity, base_dir=self.testdir)
+        gis.define_glacier_region(gdir)
+        gis.glacier_masks(gdir)
+        target_var = 'topo'
+        gis.gridded_data_var_to_geotiff(gdir, varname=target_var)
+        gtiff_path = os.path.join(gdir.dir, target_var+'.tif')
+        assert os.path.exists(gtiff_path)
+        gridded_ds = salem.open_xr_dataset(gdir.get_filepath('gridded_data'))
+        gridded_topo = gridded_ds[target_var]
+        gtiff_ds = salem.open_xr_dataset(gtiff_path)
+        assert gridded_ds.salem.grid == gtiff_ds.salem.grid
+        assert np.allclose(gridded_topo.data, gtiff_ds.data)
 
 class TestCenterlines(unittest.TestCase):
 
