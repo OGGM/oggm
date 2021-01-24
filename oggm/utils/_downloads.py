ca37435dbacf379eb5ba5845ffa6372216a885e4
Fabien Maussion <fabien.maussion@uibk.ac.at>
2021-01-24 23:57:57 +0100

    Change baselines

--- oggm/utils/_downloads.py
+++ oggm/utils/_downloads.py
@@ -71,7 +71,7 @@ logger = logging.getLogger('.'.join(__name__.split('.')[:-1]))
 # The given commit will be downloaded from github and used as source for
 # all sample data
 SAMPLE_DATA_GH_REPO = 'OGGM/oggm-sample-data'
-SAMPLE_DATA_COMMIT = '759c2de19b7370d64b600fb89dd52c4c0581d1f0'
+SAMPLE_DATA_COMMIT = '69c8c376a75c5eb53fa2c657948e837f2b3a1d6e'
 
 GDIR_URL = 'https://cluster.klima.uni-bremen.de/~oggm/gdirs/oggm_v1.1/'
 DEMO_GDIR_URL = 'https://cluster.klima.uni-bremen.de/~oggm/demo_gdirs/'
