ccc44dbb16b9fbf6cefc617dc3ded84d9b153675
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2022-09-04 10:44:31 +0200

    Update test image, baseline commit and actions (#1456)

--- oggm/utils/_downloads.py
+++ oggm/utils/_downloads.py
@@ -69,7 +69,7 @@ logger = logging.getLogger('.'.join(__name__.split('.')[:-1]))
 # The given commit will be downloaded from github and used as source for
 # all sample data
 SAMPLE_DATA_GH_REPO = 'OGGM/oggm-sample-data'
-SAMPLE_DATA_COMMIT = 'a27e6bd9badd53448515d36cc1574b8270c6da44'
+SAMPLE_DATA_COMMIT = 'c0c2ad2a7718ccedb23380c3df05acb3fc726840'
 
 GDIR_L1L2_URL = ('https://cluster.klima.uni-bremen.de/~oggm/gdirs/oggm_v1.4/'
                  'L1-L2_files/centerlines/')
