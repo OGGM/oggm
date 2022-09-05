b4b06789c1e72811f803dddc0d225aff6d878761
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2022-09-05 16:17:31 +0200

    Add pytest.oggm wrapper
    This is a workaround for https://github.com/pytest-dev/pytest/issues/1596
    Without this, testing an installed version of oggm is impossible.

---
+++ oggm/tests/__main__.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+HERE = os.path.dirname(__file__)
+
+def main():
+    import pytest
+    return pytest.main([HERE] + sys.argv[1:])
+
+if __name__ == "__main__":
+    sys.exit(main())
