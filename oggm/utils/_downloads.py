841519615a1f51e17ae6c893189464f7a29ba96a
Fabien Maussion <fabien.maussion@uibk.ac.at>
2023-02-24 17:48:22 +0100

    Ups mistake in links

--- oggm/utils/_downloads.py
+++ oggm/utils/_downloads.py
@@ -69,7 +69,7 @@ logger = logging.getLogger('.'.join(__name__.split('.')[:-1]))
 # The given commit will be downloaded from github and used as source for
 # all sample data
 SAMPLE_DATA_GH_REPO = 'OGGM/oggm-sample-data'
-SAMPLE_DATA_COMMIT = 'b951ac488f86ed376aaffaedded92726707b3c89'
+SAMPLE_DATA_COMMIT = '0117d838716e14cc37e25d9cca0fdf731d264f96'
 
 CHECKSUM_URL = 'https://cluster.klima.uni-bremen.de/data/downloads.sha256.hdf'
 CHECKSUM_VALIDATION_URL = CHECKSUM_URL + '.sha256'
