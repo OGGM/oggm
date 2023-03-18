cfb11d1505077521c9e419aaa1f9beb5ffe6549e
Fabien Maussion <fabien.maussion@uibk.ac.at>
2023-03-18 19:23:54 +0500

    Update hash

--- oggm/utils/_downloads.py
+++ oggm/utils/_downloads.py
@@ -69,7 +69,7 @@ logger = logging.getLogger('.'.join(__name__.split('.')[:-1]))
 # The given commit will be downloaded from github and used as source for
 # all sample data
 SAMPLE_DATA_GH_REPO = 'OGGM/oggm-sample-data'
-SAMPLE_DATA_COMMIT = '398729eef5f8baeb01d466a2d3038ee7ccd2724d'
+SAMPLE_DATA_COMMIT = 'ca9b53c9f00ea85699cca53ffe3503d9cdd85405'
 
 CHECKSUM_URL = 'https://cluster.klima.uni-bremen.de/data/downloads.sha256.hdf'
 CHECKSUM_VALIDATION_URL = CHECKSUM_URL + '.sha256'
