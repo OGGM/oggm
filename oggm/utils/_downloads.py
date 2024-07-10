afe4ae385392e55b8168a328c9ef1e5c2920c847
Fabien Maussion <fabien.maussion@bristol.ac.uk>
2024-07-10 11:31:06 +0900

    update graphics test

--- oggm/utils/_downloads.py
+++ oggm/utils/_downloads.py
@@ -62,7 +62,7 @@ logger = logging.getLogger('.'.join(__name__.split('.')[:-1]))
 # The given commit will be downloaded from github and used as source for
 # all sample data
 SAMPLE_DATA_GH_REPO = 'OGGM/oggm-sample-data'
-SAMPLE_DATA_COMMIT = '56aa8f23e3b450e6f56a8d4e8a5cdb58e03c9cc1'
+SAMPLE_DATA_COMMIT = 'c6b64892b6da37b8b43c83e2cb148e9872d34f48'
 
 CHECKSUM_URL = 'https://cluster.klima.uni-bremen.de/data/downloads.sha256.hdf'
 CHECKSUM_VALIDATION_URL = CHECKSUM_URL + '.sha256'
