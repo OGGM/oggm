0fcd0aa5c66142c17c0579d0b9e8caf924f7f56d
Fabien Maussion <fabien.maussion@bristol.ac.uk>
2025-11-20 10:56:49 +0000

    zwischencommit

--- oggm/core/dynamic_spinup.py
+++ oggm/core/dynamic_spinup.py
@@ -453,9 +453,9 @@ def run_dynamic_spinup(gdir, init_model_filesuffix=None, init_model_yr=None,
             if delete_area_min_h:
                 ovars.remove('area_min_h')
 
-            if type(ds) == tuple:
+            if isinstance(ds, tuple):
                 ds = ds[0]
-            model_area_km2 = ds.area_m2_min_h.loc[target_yr].values * 1e-6
+            model_area_km2 = ds.area_min_h_m2.loc[target_yr].values * 1e-6
             model_volume_km3 = ds.volume_m3.loc[target_yr].values * 1e-9
         else:
             # only run to rgi date and extract values
