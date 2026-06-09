308a59d4306e8957992598e27314926b57f0187f
gampnico <45390064+gampnico@users.noreply.github.com>
2026-06-09 14:15:15 +0100

    fix: force zarr conversion

--- oggm/core/centerlines.py
+++ oggm/core/centerlines.py
@@ -1320,6 +1320,8 @@ def compute_downstream_bedshape(gdir):
     out['bedshapes'] = bs
     out['surface_h'] = hgts
     out['w0s'] = w0s
+    from oggm.utils.geozarr import convert_pickles_to_datatree
+    convert_pickles_to_datatree({'downstream_line': gdir.read_store('downstream_line')})
     gdir.write_store(out, 'downstream_line')
 
 
